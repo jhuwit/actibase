833126cd32225f302ee05649b1ed8dd0ed6d31ba
muschellij2 <muschellij2@gmail.com>
2026-05-13 13:44:10 -0400

    added some info for the raw data

--- R/acti_raw_data.R
+++ R/acti_raw_data.R
@@ -4,8 +4,8 @@
 #' @format A data.frame with the columns
 #' \describe{
 #' \item{time}{time column}
-#' \item{X}{X column}
-#' \item{Y}{Y column}
-#' \item{Z}{Y column}
+#' \item{X}{X-axis column}
+#' \item{Y}{Y-axis column}
+#' \item{Z}{Z-axis column}
 #' }
 "acti_raw_data"
