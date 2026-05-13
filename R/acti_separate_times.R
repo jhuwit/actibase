995fe8c919e1ffd081302793154e19786b84682b
muschellij2 <muschellij2@gmail.com>
2026-05-13 15:59:23 -0400

    added example and different time separators

--- R/acti_separate_times.R
+++ R/acti_separate_times.R
@@ -1,8 +1,3 @@
-# Legacy overlay helpers.
-# These functions are kept out of the exported actibase baseline API on
-# purpose. Move them into a downstream overlay package if they are still
-# needed.
-
 #' Separate Times into Date, Hour, and Minute
 #'
 #' @param data a `data.frame` with a `time` column
@@ -12,9 +7,13 @@
 #'
 #' @examples
 #' acti_separate_time(acti_raw_data)
+#' acti_create_date(acti_raw_data)
+#' acti_create_hour(acti_raw_data)
+#' acti_create_minute(acti_raw_data)
 acti_separate_time = function(data) {
   time = minute = hour = day = date = NULL
   rm(list = c("minute", "day", "hour", "date", "time"))
+  data = acti_standardize_data(data, subset_xyz = FALSE, check_xyz = FALSE)
   data = data %>%
     dplyr::mutate(
       date = lubridate::floor_date(time, "1 day"),
@@ -29,4 +28,44 @@ acti_separate_time = function(data) {
       day = as.numeric(difftime(date, min(date), units = "days") + 1)
     )
   data
+}
+
+#' @export
+#' @rdname acti_separate_time
+acti_create_date = function(data) {
+  time = date = NULL
+  rm(list = c("date", "time"))
+  data = acti_standardize_data(data, subset_xyz = FALSE, check_xyz = FALSE)
+  data = data %>%
+    dplyr::mutate(
+      date = lubridate::floor_date(time, "1 day"),
+      date = lubridate::as_date(date)
+    )
+}
+
+#' @export
+#' @rdname acti_separate_time
+acti_create_hour = function(data) {
+  time = minute = hour = day = date = NULL
+  rm(list = c("minute", "day", "hour", "date", "time"))
+  data = acti_standardize_data(data, subset_xyz = FALSE, check_xyz = FALSE)
+  data = data %>%
+    dplyr::mutate(
+      hour = lubridate::floor_date(time, "1 hour"),
+      hour = hms::as_hms(hour)
+    )
+}
+
+
+#' @export
+#' @rdname acti_separate_time
+acti_create_minute = function(data) {
+  time = minute = hour = day = date = NULL
+  rm(list = c("minute", "day", "hour", "date", "time"))
+  data = acti_standardize_data(data, subset_xyz = FALSE, check_xyz = FALSE)
+  data = data %>%
+    dplyr::mutate(
+      minute = lubridate::floor_date(time, "1 minute"),
+      minute = hms::as_hms(minute)
+    )
 }
--- example.R
+++ example.R
@@ -7,23 +7,34 @@ stepcount::stepcount_check()
 
 library(tidyverse)
 library(activerse)
-# devtools::load_all(".")
 path = "~/Dropbox/Data/wearable_pilot/data/raw/ag_axivity_test/John/114890_0000000000.cwa"
 
 # path = "inst/extdata/TAS1H30182785_2019-09-17.gt3x.gz"
 data = acti_read_cwa(path)
-# data = acti_calibrate(data)
+function(data, calibrate = FALSE) {
+  if (calibrate) {
+    data = acti_calibrate(data)
+  }
 
-# already resampled
-processed = acti_process(data)
-get_transformations(processed)
+  # already resampled
+  processed = acti_process(data)
+  trans = get_transformations(processed)
 
-res = create_day_inclusion(processed)
-data = data %>%
-  mutate(date = as.Date(time)) %>%
-  left_join(res)
+  steps = acti_calculate_stepcount(data)
+  step_trans = get_transformations(steps)
+  processed = processed %>%
+    dplyr::full_join(steps)
 
-steps = acti_calculate_stepcount(data)
+  res = create_day_inclusion(processed, min_required = 1368L)
+  processed = processed %>%
+    mutate(date = as.Date(time)) %>%
+    left_join(res)
+  processed %>%
+    mutate(log10AC = log(counts)) %>%
+
+    summarise
+}
+
 
 
 
